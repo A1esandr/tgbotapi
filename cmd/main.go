3d66e18cc4a43623f28f5f0bde015743ffafa960
A1esandr <a1exswan001@gmail.com>
2021-10-21 00:03:28 +0500

    added flag

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"os"
@@ -8,8 +9,14 @@ import (
 	"github.com/A1esandr/tgbotapi"
 )
 
+var tokenFlag = flag.String("token", "", "Bot token")
+
 func main() {
+	flag.Parse()
 	token := os.Getenv("TOKEN")
+	if token == "" && tokenFlag != nil {
+		token = *tokenFlag
+	}
 	if token == "" {
 		log.Fatal("token is empty!")
 	}
